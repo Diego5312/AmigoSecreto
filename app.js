a5fe625782e09484e5a66501591a09313d12d2c3
Diego5312 <diego_5312@outlook.es>
2025-02-01 17:23:03 -0600

    Creamos la funcion para agregar amigos y lo mostramos en forma de lista, ademas validamos que el campo no este vacio

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// definimos la lista que almacena los amigos
+let amigos = []
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+  
+    // Validar que el nombre no esté vacío 
+    if (nombre.trim() === "") {
+      alert("Por favor, ingresa un nombre.");
+      return;
+    }
+  
+    amigos.push(nombre);
+  
+    // Obtener la lista y actualizar su contenido
+    let listaActualizada = document.getElementById('listaAmigos');
+  
+    // Mostrar los nombres de amigos en una lista (ejemplo)
+    let listaHTML = "<ul>";
+    for (let amigo of amigos) {
+      listaHTML += "<li>" + amigo + "</li>";
+    }
+    listaHTML += "</ul>";
+  
+    listaActualizada.innerHTML = listaHTML;
+  
+    return amigos;
+  }
