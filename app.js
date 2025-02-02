a25d957deb799ccb13b20edaf610408fc7c1b293
Diego5312 <diego_5312@outlook.es>
2025-02-01 21:43:11 -0600

    creamos la funcion para seleccionar un amigo al azar y la llamamos mediante la funcion sortearAmigo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function agregarAmigo() {
       alert("Por favor, ingresa un nombre.");
       return;
     }
-  
+    
     amigos.push(nombre);
   
     // Obtener la lista y actualizar su contenido
@@ -25,4 +25,22 @@ function agregarAmigo() {
     listaActualizada.innerHTML = listaHTML;
   
     return amigos;
+  }
+
+  function sorteo(){
+    // Obtenemos el numero total de amigos
+    let numeroAmigos = amigos.length;
+    if (numeroAmigos > 0 ) { // Validamos si la lista esta vacia 
+        let numeroRandom = Math.floor(Math.random()*numeroAmigos);
+        let resultadoAmigo = amigos[numeroRandom];
+        let mostrandoResultado = document.getElementById('resultado')
+        mostrandoResultado.innerHTML = resultadoAmigo;
+    } else{
+        alert('Tu lista de amigos esta vacia, intenta agregar algunos nombres!')
+    }
+    
+
+  }
+  function sortearAmigo() {
+    sorteo();
   }
